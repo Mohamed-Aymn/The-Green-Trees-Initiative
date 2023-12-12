c81b8312f6e65427b37be9612204abe7b04a0292
Mohamed Aymn <mohamedaymn218@gmail.com>
2023-12-12 21:18:33 +0200

    feat: initial js folder structure

---
+++ src/js/app.js
@@ -0,0 +1,34 @@
+import common from "./utils/common.js";
+
+document.addEventListener('DOMContentLoaded', function () {
+    // common functionalities accross all pages
+    common();
+
+    // js logic for specific pages
+    const currentPage = window.location.pathname;
+    switch (true) {
+        case currentPage === '/':
+        case currentPage.includes('/index'):
+            import('./modules/home.js').then((module) => module.init());
+            break;
+
+        case currentPage.includes('/about-us'):
+            import('./modules/about-us.js').then((module) => module.init());
+            break;
+
+        case currentPage.includes('/appreciation'):
+            import('./modules/appreciation.js').then((module) => module.init());
+            break;
+
+        case currentPage.includes('/payment-first-stage'):
+            import('./modules/payment-first-stage.js').then((module) => module.init());
+            break;
+
+        case currentPage.includes('/payment-second-stage'):
+            import('./modules/payment-second-stage.js').then((module) => module.init());
+            break;
+
+        default:
+            console.error("Page not recognized");
+    }
+});
---
+++ src/js/modules/payment-first-stage.js
@@ -0,0 +1,4 @@
+export function init() {
+    console.log('page initialized.');
+    // Add home page-specific logic here
+}
