4160d764bdd8c01aed020254841df246e62da2f0
Mohamed Aymn <mohamedaymn218@gmail.com>
2023-12-15 23:19:10 +0200

    feat: mobile navbar

--- src/js/utils/layout.js
+++ src/js/utils/layout.js
@@ -1,4 +1,4 @@
-const navbar = () => {
+const navbar = async () => {
     const currentPage = window.location.pathname;
     const indexPage = currentPage.includes("index")
 
@@ -9,13 +9,44 @@ const navbar = () => {
             <li><a href="${indexPage ? "./pages/" : ""}about-us.html" class="navbar__links-container__link">About us</a></li>
             <li><a href="${indexPage ? "./pages/" : ""}payment-first-stage.html" class="navbar__links-container__link navbar__links-container__link--primary">Donate now</a></li>
         </ul>
+        <div id="navbar__mobile-icon" class="navbar__icon-container">
+            <img src="${indexPage ? "../" : "../../"}assets/photos/nav-icon.svg" alt="nav-icon" />
+        </div>
+        <div id="navbar__mobile-display">
+            <div class="navbar__mobile-close-icon-conatiner__icon-conatiner">
+                <img id="navbar__mobile-close-icon-conatiner__icon" src="${indexPage ? "../" : "../../"}assets/photos/close-icon.svg" alt="close-icon" />
+            </div>
+            <ul class="navbar__mobile-display__items">
+                <li><a href="https://github.com/gigachadteam/Very-Green-WebProject" class="navbar__links-container__link">Github</a></li>
+                <li><a href="${indexPage ? "./pages/" : ""}about-us.html" class="navbar__links-container__link">About us</a></li>
+                <li><a href="${indexPage ? "./pages/" : ""}payment-first-stage.html" class="navbar__links-container__link navbar__links-container__link--primary">Donate now</a></li>
+            </ul>
+        </div>
     `;
 
     const tempContainer = document.createElement('nav');
     tempContainer.innerHTML = HTML;
     tempContainer.classList.add("navbar");
+    const overlayDiv = document.createElement('div');
+    overlayDiv.id = 'navbar__overlay';
 
-    document.body.prepend(tempContainer)
+    await document.body.prepend(tempContainer);
+    await document.body.prepend(overlayDiv);
+
+    const navbarIcon = document.getElementById("navbar__mobile-icon");
+    const navbarBody = document.getElementById("navbar__mobile-display");
+    const navbarCloseIcon = document.getElementById("navbar__mobile-close-icon-conatiner__icon");
+    const overlay = document.getElementById("navbar__overlay");
+
+    navbarIcon.addEventListener("click", () => {
+        navbarBody.style.display = "block";
+        overlay.style.display = "block";
+    });
+
+    navbarCloseIcon.addEventListener("click", () => {
+        navbarBody.style.display = "none";
+        overlay.style.display = "none";
+    });
 };
 
 const footer = () => {
