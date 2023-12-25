423fafaa4064305f736751fed043ec262a89bbb6
Mohamed Aymn <mohamedaymn218@gmail.com>
2023-12-25 19:27:44 +0200

    feat: cookie message

--- src/js/utils/common.js
+++ src/js/utils/common.js
@@ -2,10 +2,12 @@ import { loadState } from './state.js'
 import { footer } from './footer.js'
 import { navbar } from './navbar.js'
 import { themeButton } from './themeButton.js'
+import { cookiesPermission } from './cookiesPermission.js'
 
 export const common = () => {
     loadState()
     footer()
     navbar()
     themeButton()
+    cookiesPermission()
 }
